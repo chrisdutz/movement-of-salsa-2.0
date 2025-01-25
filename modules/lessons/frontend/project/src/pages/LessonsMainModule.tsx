a8a250e00acf586f7183ccd5190d37510d445add
Christofer Dutz <christofer.dutz@c-ware.de>
2025-01-25 22:24:34 +0100

    refactor: Cleaned up the visuals on the course type list and added output to the course rates.

--- modules/lessons/frontend/project/src/pages/LessonsMainModule.tsx
+++ modules/lessons/frontend/project/src/pages/LessonsMainModule.tsx
@@ -8,6 +8,7 @@ import {CourseType, RestApplicationClient} from "../generated/tools-ui-frontend.
 import {useSelector} from "react-redux";
 import {useEffect, useState} from "react";
 import {Image} from "primereact/image";
+import {Chip} from "primereact/chip";
 
 axios.defaults.baseURL = 'http://localhost:8080';
 const restClient = new RestApplicationClient(axios);
@@ -31,8 +32,16 @@ export default function NewsMainModule() {
             {courseTypes && courseTypes.map(value => {
                 return (<Card title={value.title}>
                         <div className="flex flex-column md:flex-row gap-4 w-full">
-                            <div><Image src={value.image.imageData}/></div>
-                            <div dangerouslySetInnerHTML={{__html: value.description}}/>
+                            <div><Image width="240" height="240" src={value.image.imageData}/></div>
+                            <div className="flex flex-column gap-2">
+                                <div dangerouslySetInnerHTML={{__html: value.description}}/>
+                                <h4 className="mt-1 mb-1">Tarife:</h4>
+                                <div className="flex flex-wrap gap-2">
+                                    {value.rates.map(rate => {
+                                        return <Chip label={rate.title + " " + rate.price + "€"}/>
+                                    })}
+                                </div>
+                            </div>
                         </div>
                     </Card>
                 )})}
