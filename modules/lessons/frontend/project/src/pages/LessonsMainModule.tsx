f0d6dec009ef0ffa3edb0f865bb28b200471166c
Christofer Dutz <christofer.dutz@c-ware.de>
2025-01-26 13:07:58 +0100

    feat: Made the layout output multiple courses in a row with automatic line-break if the width gets too small.

--- modules/lessons/frontend/project/src/pages/LessonsMainModule.tsx
+++ modules/lessons/frontend/project/src/pages/LessonsMainModule.tsx
@@ -53,101 +53,103 @@ export default function NewsMainModule() {
                             <div className="flex flex-column gap-2">
                                 <div dangerouslySetInnerHTML={{__html: value.description}}/>
                                 <h4 className="mt-1 mb-1">Kommende Kurse:</h4>
-                                {upAndRunningCourses[value.code] && upAndRunningCourses[value.code].length > 0 ? (
-                                    upAndRunningCourses[value.code].map(course => {
-                                        // If all lessons are on the same weekday the title should be "Tuesdays (Weekly)", if it's on multiple days, "Saturday, Sunday (Workshop)"
-                                        let courseStarted = false
-                                        const weekdays:number[] = []
-                                        const locations:string[] = []
-                                        const startTimes:string[] = []
-                                        const endTimes:string[] = []
-                                        course.lessons.forEach(lesson => {
-                                            const startTime = new Date(lesson.startTime);
-                                            const endTime = new Date(lesson.endTime);
-                                            if (startTime.getTime() < Date.now()) {
-                                                courseStarted = true
-                                            }
-                                            if(!weekdays.includes(startTime.getDay())) {
-                                                weekdays.push(startTime.getDay());
-                                            }
-                                            if(!locations.includes(lesson.location)) {
-                                                locations.push(lesson.location);
-                                            }
-                                            if(!startTimes.includes(startTime.toLocaleTimeString())) {
-                                                startTimes.push(startTime.toLocaleTimeString());
-                                            }
-                                            if(!endTimes.includes(endTime.toLocaleTimeString())) {
-                                                endTimes.push(endTime.toLocaleTimeString());
-                                            }
-                                        })
-                                        const title:string = (weekdays.length == 1) ?
-                                            `${dayNames[weekdays[0]]}s (Wöchentlich)` :
-                                            weekdays.map(weekday => dayNames[weekday]).join(", ") + " (Workshop)"
-
-                                        console.log("Collections", weekdays, locations, startTimes, endTimes)
-
-                                        let description:string = ""
-                                        // This is the case where it's a course with all lessons happening on the same day at the same time in the same location.
-                                        if ((weekdays.length == 1) && (locations.length == 1) && (startTimes.length == 1) && (endTimes.length == 1)) {
-                                            const firstLesson = course.lessons.reduce((earliest, current) =>
-                                                current.startTime < earliest.startTime ? current : earliest);
-                                            const lastLesson = course.lessons.reduce((last, current) =>
-                                                current.startTime > last.startTime ? current : last);
-                                            description += `${course.lessons.length} Termine vom ${new Date(firstLesson.startTime).toLocaleDateString([], {
-                                                day: '2-digit',
-                                                month: 'short',
-                                            })} bis ${new Date(lastLesson.startTime).toLocaleDateString([], {
-                                                day: '2-digit',
-                                                month: 'short',
-                                            })} jeweils von ${new Date(firstLesson.startTime).toLocaleTimeString([], {
-                                                hour: '2-digit',
-                                                minute: '2-digit',
-                                            })} bis ${new Date(firstLesson.endTime).toLocaleTimeString([], {
-                                                hour: '2-digit',
-                                                minute: '2-digit',
-                                            })} Uhr in: ${firstLesson.location}`
-                                        }
-                                        // Otherwise list all dates, times and locations.
-                                        else {
-                                            description += `${course.lessons.length} Termine:<ul>`
+                                <div className="flex flex-wrap gap-4 md:flex-row">
+                                    {upAndRunningCourses[value.code] && upAndRunningCourses[value.code].length > 0 ? (
+                                        upAndRunningCourses[value.code].map(course => {
+                                            // If all lessons are on the same weekday the title should be "Tuesdays (Weekly)", if it's on multiple days, "Saturday, Sunday (Workshop)"
+                                            let courseStarted = false
+                                            const weekdays:number[] = []
+                                            const locations:string[] = []
+                                            const startTimes:string[] = []
+                                            const endTimes:string[] = []
                                             course.lessons.forEach(lesson => {
-                                                description += `<li>${new Date(lesson.startTime).toLocaleDateString([], {
-                                                    weekday: 'short',
+                                                const startTime = new Date(lesson.startTime);
+                                                const endTime = new Date(lesson.endTime);
+                                                if (startTime.getTime() < Date.now()) {
+                                                    courseStarted = true
+                                                }
+                                                if(!weekdays.includes(startTime.getDay())) {
+                                                    weekdays.push(startTime.getDay());
+                                                }
+                                                if(!locations.includes(lesson.location)) {
+                                                    locations.push(lesson.location);
+                                                }
+                                                if(!startTimes.includes(startTime.toLocaleTimeString())) {
+                                                    startTimes.push(startTime.toLocaleTimeString());
+                                                }
+                                                if(!endTimes.includes(endTime.toLocaleTimeString())) {
+                                                    endTimes.push(endTime.toLocaleTimeString());
+                                                }
+                                            })
+                                            const title:string = (weekdays.length == 1) ?
+                                                `${dayNames[weekdays[0]]}s (Wöchentlich)` :
+                                                weekdays.map(weekday => dayNames[weekday]).join(", ") + " (Workshop)"
+
+                                            console.log("Collections", weekdays, locations, startTimes, endTimes)
+
+                                            let description:string = ""
+                                            // This is the case where it's a course with all lessons happening on the same day at the same time in the same location.
+                                            if ((weekdays.length == 1) && (locations.length == 1) && (startTimes.length == 1) && (endTimes.length == 1)) {
+                                                const firstLesson = course.lessons.reduce((earliest, current) =>
+                                                    current.startTime < earliest.startTime ? current : earliest);
+                                                const lastLesson = course.lessons.reduce((last, current) =>
+                                                    current.startTime > last.startTime ? current : last);
+                                                description += `${course.lessons.length} Termine vom ${new Date(firstLesson.startTime).toLocaleDateString([], {
                                                     day: '2-digit',
                                                     month: 'short',
-                                                })} von ${new Date(lesson.startTime).toLocaleTimeString([], {
+                                                })} bis ${new Date(lastLesson.startTime).toLocaleDateString([], {
+                                                    day: '2-digit',
+                                                    month: 'short',
+                                                })} jeweils von ${new Date(firstLesson.startTime).toLocaleTimeString([], {
                                                     hour: '2-digit',
                                                     minute: '2-digit',
-                                                })} bis ${new Date(lesson.endTime).toLocaleTimeString([], {
+                                                })} bis ${new Date(firstLesson.endTime).toLocaleTimeString([], {
                                                     hour: '2-digit',
                                                     minute: '2-digit',
-                                                })} Uhr`
-                                                if(locations.length > 1) {
-                                                    description += ` in: ${lesson.location}`
-                                                }
-                                                description += `</li>`
-                                            })
-                                            description += `</ul>`
-                                            if(locations.length == 1) {
-                                                description += ` in: ${locations[0]}`
+                                                })} Uhr in: ${firstLesson.location}`
                                             }
-                                        }
+                                            // Otherwise list all dates, times and locations.
+                                            else {
+                                                description += `${course.lessons.length} Termine:<ul>`
+                                                course.lessons.forEach(lesson => {
+                                                    description += `<li>${new Date(lesson.startTime).toLocaleDateString([], {
+                                                        weekday: 'short',
+                                                        day: '2-digit',
+                                                        month: 'short',
+                                                    })} von ${new Date(lesson.startTime).toLocaleTimeString([], {
+                                                        hour: '2-digit',
+                                                        minute: '2-digit',
+                                                    })} bis ${new Date(lesson.endTime).toLocaleTimeString([], {
+                                                        hour: '2-digit',
+                                                        minute: '2-digit',
+                                                    })} Uhr`
+                                                    if(locations.length > 1) {
+                                                        description += ` in: ${lesson.location}`
+                                                    }
+                                                    description += `</li>`
+                                                })
+                                                description += `</ul>`
+                                                if(locations.length == 1) {
+                                                    description += ` in: ${locations[0]}`
+                                                }
+                                            }
 
-                                        return (<Card title={title} style={{ backgroundColor: 'lightblue' }}>
-                                            {courseStarted && (
-                                                <b className="text-red-600">Achtung - Der Kurs hat bereits begonnen!</b>
-                                            )}
-                                            <div dangerouslySetInnerHTML={{__html: description}}/>
-                                            {course.closed ? (
-                                                <p className="text-red-600">Anmeldung nicht mehr möglich</p>
-                                            ):(
-                                                <p>Anmelden</p>
-                                            )}
-                                        </Card>)
-                                    })
-                                ) : (
-                                    <b>Aktuell sind neue Kurse in Vorbereitung und werden demnächst hier aufrufbar sein.</b>
-                                )}
+                                            return (<Card title={title} style={{ backgroundColor: 'lightblue' }} className="flex-1 min-w-[350px] md:max-w-[calc(33.33%-1rem)]">
+                                                {courseStarted && (
+                                                    <b className="text-red-600">Achtung - Der Kurs hat bereits begonnen!</b>
+                                                )}
+                                                <div dangerouslySetInnerHTML={{__html: description}}/>
+                                                {course.closed ? (
+                                                    <p className="text-red-600">Anmeldung nicht mehr möglich</p>
+                                                ):(
+                                                    <p>Anmelden</p>
+                                                )}
+                                            </Card>)
+                                        })
+                                    ) : (
+                                        <b>Aktuell sind neue Kurse in Vorbereitung und werden demnächst hier aufrufbar sein.</b>
+                                    )}
+                                </div>
                                 <h4 className="mt-1 mb-1">Tarife:</h4>
                                 <div className="flex flex-wrap gap-2">
                                     {value.rates.map(rate => {
